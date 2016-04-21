ce66393cc29bad8b2f750efa034886d40ad3baba
Guades <ianemsens@gmail.com>
2016-04-21 16:38:41 +0200

    front end fabulousness

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -29,4 +29,96 @@ $(document).ready(function() {
 	});
 
     initDesignCanvas();
+
+    var intervalTime = 2000;
+
+    setInterval(function() {
+        discoCycle(intervalTime);
+    },intervalTime*6);
+
+    setInterval(function() {
+        neonCycle(intervalTime);
+    },intervalTime*3)
+
+    neonCycle(intervalTime);
+    discoCycle(intervalTime);
+    
 });
+
+function discoBlack() {
+    $("#disco-target").css("background-color","rgba(0,0,0,0.4)");
+}
+
+function discoRed() {
+    $("#disco-target").css("background-color","rgba(231, 59, 53, 0.9)");
+}
+
+function discoBlue() {
+    $("#disco-target").css("background-color","rgba(103, 198, 229, 0.9)");
+}
+
+function discoYellow() {
+    $("#disco-target").css("background-color","rgba(255, 209, 34, 0.9)");
+}
+
+
+function neonBlack(){
+    $("#neon-target").css("text-shadow","0.06em 0.06em 0px rgba(0,0,0,0.4)");
+}
+
+function neonRed() {
+    $("#neon-target").css("text-shadow","0.06em 0.06em 0px rgba(231, 59, 53, 0.9)");
+}
+
+function neonYellow() {
+    $("#neon-target").css("text-shadow","0.06em 0.06em 0px rgba(255, 209, 34, 0.9)");
+}
+
+function neonBlue(){
+    $("#neon-target").css("text-shadow","0.06em 0.06em 0px rgba(103, 198, 229, 0.9)");
+}
+
+
+function discoCycle(x){
+    var intervalTime = x;
+
+    setTimeout(function() {
+        discoBlack();
+    }, intervalTime);
+
+    setTimeout(function() {
+        discoYellow();
+    }, intervalTime*2);
+
+    setTimeout(function() {
+        discoBlack();
+    }, intervalTime*3);
+
+    setTimeout(function() {
+        discoBlue();
+    }, intervalTime*4);
+
+    setTimeout(function() {
+        discoBlack();
+    }, intervalTime*5);
+
+    setTimeout(function() {
+        discoRed();
+    }, intervalTime*6);
+}
+
+function neonCycle(x){
+    var intervalTime = x/2;
+
+    setTimeout(function() {
+        neonYellow();
+    }, intervalTime);
+
+    setTimeout(function() {
+        neonBlue();
+    }, intervalTime*2);
+
+    setTimeout(function() {
+        neonRed();
+    }, intervalTime*3);
+}
